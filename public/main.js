ad49939eb2e8185af2c27b4ca0f7d8462efe41a1
panguanzhen <panguanzhen@qq.com>
2020-02-26 22:22:36 +0800

    finished

--- public/main.js
+++ public/main.js
@@ -55,13 +55,17 @@ getXML.onclick =() =>{
 getHTML.onclick =() =>{
     const request =new XMLHttpRequest()
     request.open('GET','/3.html')
-    request.onload =() =>{
-        console.log(request.response)
+    request.onreadystatechange =() =>{
+        if(request.readyState ===4){
+            if(request.status >= 200 && request.status <300){
         const div =document.createElement('div')
         div.innerHTML =request.response
         document.body.appendChild(div)
+    }else {
+        alert('加载失败')
     }
-    request.onerror =() =>{}
+}
+}
     request.send()
 }
 
@@ -69,13 +73,17 @@ getHTML.onclick =() =>{
 getJS.onclick =() =>{
     const request =new XMLHttpRequest()
     request.open("GET",'/2.js')
-    request.onload =() =>{
-        console.log(request.response)
+    request.onreadystatechange =() =>{
+        if(request.readyState===4){
+            if(request.status >=200 && request.status <300){
         const script =document.createElement('script')
         script.innerHTML = request.response
         document.body.appendChild(script)
+    }else {
+        alert ('加载失败')
     }
-    request.onerror =() => {}
+}
+}
     request.send()
 }
 
